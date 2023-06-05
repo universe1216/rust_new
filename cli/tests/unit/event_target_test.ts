denoland/deno

commit d856bfd336137e1bcf81a0db9e8ad2b418ba711e

perf(ext/web): fast path for ws events (#18905)
- Do not use `ReflectHas` in `isNode`.
- Avoid copying handler array when handlers.length == 1
- Avoid searching for path target when path.length == 1

```
Linux divy-2 5.19.0-1022-gcp #24~22.04.1-Ubuntu SMP Sun Apr 23 09:51:08 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
32GiB System memory
Intel(R) Xeon(R) CPU @ 3.10GHz

# main + https://github.com/denoland/deno/pull/18904
Msg/sec: 89326.750000
Msg/sec: 90320.000000
Msg/sec: 89576.250000

# this patch
Msg/sec: 97250.000000
Msg/sec: 97125.500000
Msg/sec: 97964.500000
```

ext/web/02_event.js
@@ -482,7 +482,7 @@ function getRoot(eventTarget) {
 function isNode(
   eventTarget,
 ) {
-  return Boolean(eventTarget && ReflectHas(eventTarget, "nodeType"));
+  return eventTarget?.nodeType !== undefined;
 }
 
 // https://dom.spec.whatwg.org/#concept-shadow-including-inclusive-ancestor
@@ -734,8 +734,12 @@ function innerInvokeEventListeners(
     return found;
   }
 
+  let handlers = targetListeners[type];
+
   // Copy event listeners before iterating since the list can be modified during the iteration.
-  const handlers = ArrayPrototypeSlice(targetListeners[type]);
+  if (handlers.length > 1) {
+    handlers = ArrayPrototypeSlice(targetListeners[type]);
+  }
 
   for (let i = 0; i < handlers.length; i++) {
     const listener = handlers[i];
@@ -804,12 +808,19 @@ function innerInvokeEventListeners(
  * Ref: https://dom.spec.whatwg.org/#concept-event-listener-invoke */
 function invokeEventListeners(tuple, eventImpl) {
   const path = getPath(eventImpl);
-  const tupleIndex = ArrayPrototypeIndexOf(path, tuple);
-  for (let i = tupleIndex; i >= 0; i--) {
-    const t = path[i];
+  if (path.length === 1) {
+    const t = path[0];
     if (t.target) {
       setTarget(eventImpl, t.target);
-      break;
+    }
+  } else {
+    const tupleIndex = ArrayPrototypeIndexOf(path, tuple);
+    for (let i = tupleIndex; i >= 0; i--) {
+      const t = path[i];
+      if (t.target) {
+        setTarget(eventImpl, t.target);
+        break;
+      }
     }
   }
 

cli/tests/testdata/run/event_listener_error_immediate_exit.ts.out
@@ -3,8 +3,4 @@ error: Uncaught Error: bar
   throw new Error("bar");
         ^
     at [WILDCARD]/event_listener_error_immediate_exit.ts:4:9[WILDCARD]
-    at innerInvokeEventListeners (ext:deno_web/02_event.js:785:7)
-    at invokeEventListeners (ext:deno_web/02_event.js:825:5)
-    at dispatch (ext:deno_web/02_event.js:694:9)
-    at dispatchEvent (ext:deno_web/02_event.js:1086:12)
     at [WILDCARD]/event_listener_error_immediate_exit.ts:11:1

cli/tests/testdata/run/event_listener_error_immediate_exit_worker.ts.out
@@ -2,11 +2,7 @@
 error: Uncaught (in worker "") Error: bar
   throw new Error("bar");
         ^
-    at [WILDCARD]/event_listener_error_immediate_exit.ts:4:9
-    at innerInvokeEventListeners (ext:deno_web/02_event.js:785:7)
-    at invokeEventListeners (ext:deno_web/02_event.js:825:5)
-    at dispatch (ext:deno_web/02_event.js:694:9)
-    at dispatchEvent (ext:deno_web/02_event.js:1086:12)
+    at [WILDCARD]/event_listener_error_immediate_exit.ts:4:9[WILDCARD]
     at [WILDCARD]/event_listener_error_immediate_exit.ts:11:1
 error: Uncaught (in promise) Error: Unhandled error in child worker.
     at [WILDCARD]

universe1216/rust_new

commit 43d02b1f7300051de7c5aebbab8083b20818e311

fix(ext/web): Copy EventTarget list before dispatch (#19360)
Related issue: https://github.com/denoland/deno/issues/19358.

This is a regression that seems to have been introduced in
https://github.com/denoland/deno/pull/18905. It looks to have been a
performance optimization.

The issue is probably easiest described with some code:
```ts
const target = new EventTarget();
const event = new Event("foo");
target.addEventListener("foo", () => {
  console.log('base');
  target.addEventListener("foo", () => {
    console.log('nested');
  });
});
target.dispatchEvent(event);
```
Essentially, the second event listener is being attached while the `foo`
event is still being dispatched. It should then not fire that second
event listener, but Deno currently does.

ext/web/02_event.js
@@ -737,13 +737,14 @@ function innerInvokeEventListeners(
   }
 
   let handlers = targetListeners[type];
+  const handlersLength = handlers.length;
 
   // Copy event listeners before iterating since the list can be modified during the iteration.
-  if (handlers.length > 1) {
+  if (handlersLength > 1) {
     handlers = ArrayPrototypeSlice(targetListeners[type]);
   }
 
-  for (let i = 0; i < handlers.length; i++) {
+  for (let i = 0; i < handlersLength; i++) {
     const listener = handlers[i];
 
     let capture, once, passive;

cli/tests/unit/event_target_test.ts
@@ -245,6 +245,20 @@ Deno.test(function eventTargetDispatchShouldSetTargetInListener() {
   assertEquals(called, true);
 });
 
+Deno.test(function eventTargetDispatchShouldFireCurrentListenersOnly() {
+  const target = new EventTarget();
+  const event = new Event("foo");
+  let callCount = 0;
+  target.addEventListener("foo", () => {
+    ++callCount;
+    target.addEventListener("foo", () => {
+      ++callCount;
+    });
+  });
+  target.dispatchEvent(event);
+  assertEquals(callCount, 1);
+});
+
 Deno.test(function eventTargetAddEventListenerGlobalAbort() {
   return new Promise((resolve) => {
     const c = new AbortController();