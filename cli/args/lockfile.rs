denoland/deno

commit 448fe67b7a2142f62332b651f9d215534dceb1f5

feat(vendor): support modifying remote files in vendor folder without checksum errors (#23979)
Includes:

* https://github.com/denoland/deno_graph/pull/486
* https://github.com/denoland/deno_graph/pull/488
* https://github.com/denoland/deno_lockfile/pull/25
* https://github.com/denoland/deno_lockfile/pull/22
* https://github.com/denoland/deno_graph/pull/483
* https://github.com/denoland/deno_graph/pull/470

cli/args/lockfile.rs
@@ -2,10 +2,13 @@
 
 use std::path::PathBuf;
 
+use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
 use deno_runtime::deno_node::PackageJson;
 
 use crate::args::ConfigFile;
+use crate::cache;
+use crate::util::fs::atomic_write_file;
 use crate::Flags;
 
 use super::DenoSubcommand;
@@ -13,7 +16,6 @@ use super::InstallFlags;
 use super::InstallKind;
 
 pub use deno_lockfile::Lockfile;
-pub use deno_lockfile::LockfileError;
 
 pub fn discover(
   flags: &Flags,
@@ -54,6 +56,34 @@ pub fn discover(
     },
   };
 
-  let lockfile = Lockfile::new(filename, flags.lock_write)?;
+  let lockfile = if flags.lock_write {
+    Lockfile::new_empty(filename, true)
+  } else {
+    read_lockfile_at_path(filename)?
+  };
   Ok(Some(lockfile))
+}
+
+pub fn read_lockfile_at_path(filename: PathBuf) -> Result<Lockfile, AnyError> {
+  match std::fs::read_to_string(&filename) {
+    Ok(text) => Ok(Lockfile::with_lockfile_content(filename, &text, false)?),
+    Err(err) if err.kind() == std::io::ErrorKind::NotFound => {
+      Ok(Lockfile::new_empty(filename, false))
+    }
+    Err(err) => Err(err).with_context(|| {
+      format!("Failed reading lockfile '{}'", filename.display())
+    }),
+  }
+}
+
+pub fn write_lockfile_if_has_changes(
+  lockfile: &Lockfile,
+) -> Result<(), AnyError> {
+  let Some(bytes) = lockfile.resolve_write_bytes() else {
+    return Ok(()); // nothing to do
+  };
+  // do an atomic write to reduce the chance of multiple deno
+  // processes corrupting the file
+  atomic_write_file(&lockfile.filename, bytes, cache::CACHE_PERM)
+    .context("Failed writing lockfile.")
 }

cli/lsp/language_server.rs
@@ -85,6 +85,7 @@ use super::tsc::TsServer;
 use super::urls;
 use crate::args::create_default_npmrc;
 use crate::args::get_root_cert_store;
+use crate::args::write_lockfile_if_has_changes;
 use crate::args::CaData;
 use crate::args::CacheSetting;
 use crate::args::CliOptions;
@@ -247,12 +248,13 @@ impl LanguageServer {
         .await?;
       graph_util::graph_valid(
         &graph,
-        factory.fs().clone(),
+        factory.fs(),
         &roots,
         graph_util::GraphValidOptions {
           is_vendoring: false,
           follow_type_only: true,
           check_js: false,
+          exit_lockfile_errors: false,
         },
       )?;
 
@@ -260,8 +262,8 @@ impl LanguageServer {
       // found after caching
       if let Some(lockfile) = cli_options.maybe_lockfile() {
         let lockfile = lockfile.lock();
-        if let Err(err) = lockfile.write() {
-          lsp_warn!("Error writing lockfile: {:#}", err);
+        if let Err(err) = write_lockfile_if_has_changes(&lockfile) {
+          lsp_warn!("{:#}", err);
         }
       }
 

cli/module_loader.rs
@@ -9,6 +9,7 @@ use std::str;
 use std::sync::Arc;
 
 use crate::args::jsr_url;
+use crate::args::write_lockfile_if_has_changes;
 use crate::args::CliOptions;
 use crate::args::DenoSubcommand;
 use crate::args::TsTypeLib;
@@ -20,7 +21,6 @@ use crate::factory::CliFactory;
 use crate::graph_container::MainModuleGraphContainer;
 use crate::graph_container::ModuleGraphContainer;
 use crate::graph_container::ModuleGraphUpdatePermit;
-use crate::graph_util::graph_lock_or_exit;
 use crate::graph_util::CreateGraphOptions;
 use crate::graph_util::ModuleGraphBuilder;
 use crate::node;
@@ -173,13 +173,9 @@ impl ModuleLoadPreparer {
 
     self.module_graph_builder.graph_roots_valid(graph, roots)?;
 
-    // If there is a lockfile...
+    // write the lockfile if there is one
     if let Some(lockfile) = &self.lockfile {
-      let mut lockfile = lockfile.lock();
-      // validate the integrity of all the modules
-      graph_lock_or_exit(graph, &mut lockfile);
-      // update it with anything new
-      lockfile.write().context("Failed writing lockfile.")?;
+      write_lockfile_if_has_changes(&lockfile.lock())?;
     }
 
     drop(_pb_clear_guard);

cli/tools/info.rs
@@ -7,7 +7,6 @@ use std::fmt::Write;
 
 use deno_ast::ModuleSpecifier;
 use deno_core::anyhow::bail;
-use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
 use deno_core::resolve_url_or_path;
 use deno_core::serde_json;
@@ -26,11 +25,12 @@ use deno_semver::npm::NpmPackageReqReference;
 use deno_semver::package::PackageNv;
 use deno_terminal::colors;
 
+use crate::args::write_lockfile_if_has_changes;
 use crate::args::Flags;
 use crate::args::InfoFlags;
 use crate::display;
 use crate::factory::CliFactory;
-use crate::graph_util::graph_lock_or_exit;
+use crate::graph_util::graph_exit_lock_errors;
 use crate::npm::CliNpmResolver;
 use crate::npm::ManagedCliNpmResolver;
 use crate::util::checksum;
@@ -68,13 +68,10 @@ pub async fn info(flags: Flags, info_flags: InfoFlags) -> Result<(), AnyError> {
       .create_graph_with_loader(GraphKind::All, vec![specifier], &mut loader)
       .await?;
 
-    // If there is a lockfile...
+    // write out the lockfile if there is one
     if let Some(lockfile) = &maybe_lockfile {
-      let mut lockfile = lockfile.lock();
-      // validate the integrity of all the modules
-      graph_lock_or_exit(&graph, &mut lockfile);
-      // update it with anything new
-      lockfile.write().context("Failed writing lockfile.")?;
+      graph_exit_lock_errors(&graph);
+      write_lockfile_if_has_changes(&lockfile.lock())?;
     }
 
     if info_flags.json {
@@ -668,18 +665,6 @@ impl<'a> GraphDisplayContext<'a> {
       }
       ModuleError::Missing(_, _) | ModuleError::MissingDynamic(_, _) => {
         self.build_error_msg(specifier, "(missing)")
-      }
-      ModuleError::MissingWorkspaceMemberExports { .. } => {
-        self.build_error_msg(specifier, "(missing exports)")
-      }
-      ModuleError::UnknownExport { .. } => {
-        self.build_error_msg(specifier, "(unknown export)")
-      }
-      ModuleError::UnknownPackage { .. } => {
-        self.build_error_msg(specifier, "(unknown package)")
-      }
-      ModuleError::UnknownPackageReq { .. } => {
-        self.build_error_msg(specifier, "(unknown package constraint)")
       }
     }
   }

cli/tools/installer.rs
@@ -1,6 +1,7 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
 use crate::args::resolve_no_prompt;
+use crate::args::write_lockfile_if_has_changes;
 use crate::args::AddFlags;
 use crate::args::CaData;
 use crate::args::Flags;
@@ -266,7 +267,7 @@ async fn install_local(
   crate::module_loader::load_top_level_deps(&factory).await?;
 
   if let Some(lockfile) = factory.cli_options().maybe_lockfile() {
-    lockfile.lock().write()?;
+    write_lockfile_if_has_changes(&lockfile.lock())?;
   }
 
   Ok(())

cli/worker.rs
@@ -7,7 +7,6 @@ use std::sync::Arc;
 
 use deno_ast::ModuleSpecifier;
 use deno_core::anyhow::bail;
-use deno_core::anyhow::Context;
 use deno_core::error::AnyError;
 use deno_core::futures::FutureExt;
 use deno_core::parking_lot::Mutex;
@@ -48,6 +47,7 @@ use deno_terminal::colors;
 use tokio::select;
 
 use crate::args::package_json::PackageJsonDeps;
+use crate::args::write_lockfile_if_has_changes;
 use crate::args::DenoSubcommand;
 use crate::args::StorageKeyResolver;
 use crate::errors;
@@ -533,10 +533,7 @@ impl CliMainWorkerFactory {
         // For npm binary commands, ensure that the lockfile gets updated
         // so that we can re-use the npm resolution the next time it runs
         // for better performance
-        lockfile
-          .lock()
-          .write()
-          .context("Failed writing lockfile.")?;
+        write_lockfile_if_has_changes(&lockfile.lock())?;
       }
 
       (node_resolution.into_url(), is_main_cjs)

universe1216/rust_new

commit 8619178f89e471fcf54044d5225c1e1a0c6d1b67

chore: set lockfile as having no content changes after write (#24023)
Slight perf regression when updating deno_lockfile in
https://github.com/denoland/deno/pull/23979

cli/args/lockfile.rs
@@ -77,7 +77,7 @@ pub fn read_lockfile_at_path(filename: PathBuf) -> Result<Lockfile, AnyError> {
 }
 
 pub fn write_lockfile_if_has_changes(
-  lockfile: &Lockfile,
+  lockfile: &mut Lockfile,
 ) -> Result<(), AnyError> {
   let Some(bytes) = lockfile.resolve_write_bytes() else {
     return Ok(()); // nothing to do
@@ -85,5 +85,7 @@ pub fn write_lockfile_if_has_changes(
   // do an atomic write to reduce the chance of multiple deno
   // processes corrupting the file
   atomic_write_file(&lockfile.filename, bytes, cache::CACHE_PERM)
-    .context("Failed writing lockfile.")
+    .context("Failed writing lockfile.")?;
+  lockfile.has_content_changed = false;
+  Ok(())
 }

cli/lsp/language_server.rs
@@ -272,8 +272,8 @@ impl LanguageServer {
       // Update the lockfile on the file system with anything new
       // found after caching
       if let Some(lockfile) = cli_options.maybe_lockfile() {
-        let lockfile = lockfile.lock();
-        if let Err(err) = write_lockfile_if_has_changes(&lockfile) {
+        let mut lockfile = lockfile.lock();
+        if let Err(err) = write_lockfile_if_has_changes(&mut lockfile) {
           lsp_warn!("{:#}", err);
         }
       }

cli/module_loader.rs
@@ -175,7 +175,7 @@ impl ModuleLoadPreparer {
 
     // write the lockfile if there is one
     if let Some(lockfile) = &self.lockfile {
-      write_lockfile_if_has_changes(&lockfile.lock())?;
+      write_lockfile_if_has_changes(&mut lockfile.lock())?;
     }
 
     drop(_pb_clear_guard);

cli/tools/info.rs
@@ -71,7 +71,7 @@ pub async fn info(flags: Flags, info_flags: InfoFlags) -> Result<(), AnyError> {
     // write out the lockfile if there is one
     if let Some(lockfile) = &maybe_lockfile {
       graph_exit_lock_errors(&graph);
-      write_lockfile_if_has_changes(&lockfile.lock())?;
+      write_lockfile_if_has_changes(&mut lockfile.lock())?;
     }
 
     if info_flags.json {

cli/tools/installer.rs
@@ -267,7 +267,7 @@ async fn install_local(
   crate::module_loader::load_top_level_deps(&factory).await?;
 
   if let Some(lockfile) = factory.cli_options().maybe_lockfile() {
-    write_lockfile_if_has_changes(&lockfile.lock())?;
+    write_lockfile_if_has_changes(&mut lockfile.lock())?;
   }
 
   Ok(())

cli/worker.rs
@@ -533,7 +533,7 @@ impl CliMainWorkerFactory {
         // For npm binary commands, ensure that the lockfile gets updated
         // so that we can re-use the npm resolution the next time it runs
         // for better performance
-        write_lockfile_if_has_changes(&lockfile.lock())?;
+        write_lockfile_if_has_changes(&mut lockfile.lock())?;
       }
 
       (node_resolution.into_url(), is_main_cjs)